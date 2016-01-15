63e0e1100cfdff46b6f36a4b450967c84a898df1
Jake VanderPlas <jakevdp@gmail.com>
2016-01-15 08:09:58 -0800

    clarify wpca code

--- wpca.py
+++ wpca.py
@@ -36,11 +36,12 @@ def pca(X, ncomp):
     return U[:, :ncomp], s[:ncomp, None] * VT[:ncomp], s[:ncomp] ** 2 / nobs
 
 
-def wpca(X, ncomp, W=None, xi=0):
+def wpca_delchambre(X, ncomp, W=None, xi=0):
     """Weighted Principal Component Analysis
 
     This is a direct implementation of weighted PCA based on the eigenvalue
-    decomposition of the weighted covariance matrix [1]_.
+    decomposition of the weighted covariance matrix following 
+    Delchambre (2014) [1]_.
 
     Parameters
     ----------
@@ -65,21 +66,31 @@ def wpca(X, ncomp, W=None, xi=0):
         
     Notes
     -----
-    dot(P, C) is the best approximation to the input X
+    Although Delchambre (2014) suggests an iterative power method, this
+    iteration is not essential to the algorithm. Fundamentally the algorithm
+    is simply finding the standard eigenvectors of the weighted covariance
+    matrix, and so we use typical scipy tools here.
 
     References
     ----------
     .. [1] Delchambre, L. MNRAS 2014 446 (2): 3545-3555 (2014)
            http://arxiv.org/abs/1412.4533
     """
-    if W is None:
-        W = np.ones(X.shape)
     nvar, nobs = X.shape
+
+    if W is None:
+        W = np.ones((nvar, nobs))
+    
+    if W.shape != X.shape:
+        raise ValueError("shape of W and X must match")
+
+    # compute the weighted covariance matrix
     Ws = W.sum(1, keepdims=True)
     XW = X * W
     covar = (Ws @ Ws.T) ** xi * (XW @ XW.T) / (W @ W.T)
     covar[np.isnan(covar)] = 0
     
+    # diagonalize the weighted covariance
     evals, evecs = linalg.eigh(covar, eigvals=(nvar - ncomp, nvar - 1))
     sigma = evals[::-1]
     P = evecs[:, ::-1]
@@ -88,8 +99,8 @@ def wpca(X, ncomp, W=None, xi=0):
     # TODO: can this be done more efficiently?
     C = np.zeros((ncomp, nobs))
     for i in range(nobs):
-        w = W[:, i] ** 2
-        C[:, i] = np.linalg.solve((P.T * w) @ P, (P.T * w) @ X[:, i])
+        W2 = np.diag(W[:, i] ** 2)
+        C[:, i] = np.linalg.solve(P.T @ W2 @ P, P.T @ W2 @ X[:, i])
 
     return P, C, sigma
 
@@ -111,13 +122,13 @@ def assert_columns_allclose_upto_sign(A, B, *args, **kwargs):
                     norm_sign(B), *args, **kwargs)
 
 
-def test_wpca_vs_pca():
+def test_wpca_delchambre_unweighted():
     rand = np.random.RandomState(0)
     X = rand.rand(5, 100)
 
     def check_results(ncomp):
         P1, C1, S1 = pca(X, ncomp)
-        P2, C2, S2 = wpca(X, ncomp)
+        P2, C2, S2 = wpca_delchambre(X, ncomp)
         assert_allclose(S1, S2)
         assert_columns_allclose_upto_sign(P1, P2)
         assert_columns_allclose_upto_sign(C1.T, C2.T)
