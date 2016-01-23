5f5ccd23d497ecbbd385af98457150463341d23d
Jake VanderPlas <jakevdp@gmail.com>
2016-01-22 21:20:41 -0800

    refactor estimators

--- wpca/wpca_estimators.py
+++ wpca/wpca_estimators.py
@@ -1,7 +1,8 @@
 import numpy as np
+from scipy import linalg
 
-from .wpca import pca, wpca_delchambre
 from sklearn.base import BaseEstimator, TransformerMixin
+
 
 class PCA(BaseEstimator, TransformerMixin):
     """Principal Component Analysis
@@ -22,31 +23,29 @@ class PCA(BaseEstimator, TransformerMixin):
     def __init__(self, n_components=None):
         self.n_components = n_components
 
-    @staticmethod
-    def _compute_mean(X):
-        return X.mean(0)
-
     def fit_transform(self, X):
         if self.n_components is None:
             n_components = X.shape[1]
         else:
             n_components = self.n_components
-        self.mean_ = self._compute_mean(X)
-        P, C, sigma, var_tot = pca((X - self.mean_).T, n_components)
-        self.components_ = P.T
-        self.explained_variance_ = sigma
-        self.explained_variance_ratio_ = sigma / var_tot
-        return C.T
+
+        self.mean_ = X.mean(0)
+        U, s, VT = np.linalg.svd(X - self.mean_)
+        self.components_ = VT[:self.n_components]
+        var = s ** 2 / X.shape[0]
+        self.explained_variance_ = var[:self.n_components]
+        self.explained_variance_ratio_ = var[:self.n_components] / var.sum()
+        return s[:self.n_components] * U[:, :self.n_components]
 
     def fit(self, X):
         self.fit_transform(X)
         return self
 
     def transform(self, X):
-        return (X - self.mean_) @ self.components_.T
+        return np.dot(X - self.mean_, self.components_.T)
 
     def inverse_transform(self, X):
-        return self.mean_ + X @ self.components_
+        return self.mean_ + np.dot(X, self.components_)
 
     def reconstruct(self, X):
         return self.inverse_transform(self.transform(X))
@@ -79,65 +78,51 @@ class WPCA(BaseEstimator, TransformerMixin):
         self.xi = xi
         self.regularize = regularize
 
-    @staticmethod
-    def _compute_mean(X, weights):
-        if weights is None or np.isscalar(weights):
-            return X.mean(0)
-        else:
-            assert X.shape == weights.shape
-            return (X * weights).sum(0) / weights.sum(0)
-
-    def _fit(self, X, weights=None, compute_transform=True):
+    def fit(self, X, weights=None):
         if self.n_components is None:
-            n_components = X.shape[1]
+            n_components = min(X.shape)
         else:
             n_components = self.n_components
 
-        W = weights.T if weights is not None else weights
-        self.mean_ = self._compute_mean(X, weights)
-        P, C, sigma, var_tot =\
-            wpca_delchambre((X - self.mean_).T,
-                            n_components, W=W, xi=self.xi,
-                            compute_transform=compute_transform,
-                            regularize=self.regularize)
-        self.components_ = P.T
-        self.explained_variance_ = sigma
+        if weights is None:
+            self.mean_ = X.mean(0)
+            weights = np.ones_like(X)
+        else:
+            self.mean_ = (X * weights).sum(0) / weights.sum(0)
 
-        # XXX: a weighted covariance matrix is not necessarily
-        # positive-semidefinite, so the variance ratio can rise above 1.
-        # I have no idea what this means, or whether it's a useful concept
-        # in this generalization.
-        self.explained_variance_ratio_ = sigma / var_tot
+        # TODO: check for NaN and filter warnings
+        Ws = weights.sum(0)
+        XW = (X - self.mean_) * weights
+        covar = np.dot(XW.T, XW) / np.dot(weights.T, weights)
+        if self.xi != 0:
+            covar *= np.outer(Ws, Ws) ** self.xi
+        covar[np.isnan(covar)] = 0
 
-        if compute_transform:
-            return C.T
-
-    def fit_transform(self, X, weights=None):
-        return self._fit(X, weights, compute_transform=True)
-
-    def fit(self, X, weights=None):
-        self._fit(X, weights, compute_transform=False)
+        eigvals = (X.shape[1] - n_components, X.shape[1] - 1)
+        evals, evecs = linalg.eigh(covar, eigvals=eigvals)
+        self.components_ = evecs[:, ::-1].T
+        self.explained_variance_ = evals[::-1]
+        self.explained_variance_ratio_ = evals[::-1] / covar.trace()
         return self
 
     def transform(self, X, weights=None):
-        Xtrans = np.zeros((X.shape[0], self.n_components))
-        Xmu = X - self.mean_
-        P = self.components_
         if weights is None:
-            Xtrans = Xmu @ P.T
-        else:
-            for i in range(X.shape[0]):
-                W2 = np.diag(weights[i] ** 2)
-                if self.regularize:
-                    PWP = P @ W2 @ P.T + np.diag(X.shape[0] /
-                                                 self.explained_variance_)
-                else:
-                    PWP = P @ W2 @ P.T
-                Xtrans[i] = np.linalg.solve(PWP, P @ W2 @ Xmu[i])
-        return Xtrans
+            weights = np.ones_like(X)
+        Y = np.zeros((X.shape[0], self.components_.shape[0]))
+        for i in range(X.shape[0]):
+            W2 = weights[i] ** 2
+            cWc = np.dot(self.components_ * W2, self.components_.T)
+            cWX = np.dot(self.components_ * W2, X[i] - self.mean_)
+            if self.regularize:
+                cWc += np.diag(X.shape[0] / self.explained_variance_)
+            Y[i] = np.linalg.solve(cWc, cWX)
+        return Y
+
+    def fit_transform(self, X, weights=None):
+        return self.fit(X, weights).transform(X, weights)
 
     def inverse_transform(self, X):
-        return self.mean_ + X @ self.components_
+        return self.mean_ + np.dot(X, self.components_)
 
     def reconstruct(self, X, weights=None):
         return self.inverse_transform(self.transform(X, weights))
