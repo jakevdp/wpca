b5a2ac1cb17f11e1ea3e5098f3a09803ab4c5a44
Jake VanderPlas <jakevdp@gmail.com>
2016-01-22 22:35:35 -0800

    DOC: add doc strings to WPCA methods

--- wpca/wpca_estimators.py
+++ wpca/wpca_estimators.py
@@ -12,18 +12,45 @@ class PCA(BaseEstimator, TransformerMixin):
 
     Parameters
     ----------
-    TODO
+    n_components : int (optional)
+        Number of components to keep. If not specified, all components are kept
 
     Attributes
     ----------
-    mean_ :
-    components_ :
-    explained_variance_ :
+    components_ : array, [n_components, n_features]
+        Principal axes in feature space, representing the directions of
+        maximum variance in the data.
+
+    explained_variance_ : array, [n_components]
+        The amount of variance explained by each of the selected components.
+
+    explained_variance_ratio_ : array, [n_components]
+        Percentage of variance explained by each of the selected components.
+
+    mean_ : array, [n_features]
+        Per-feature empirical mean, estimated from the training set.
+
+    See Also
+    --------
+    - WPCA
+    - sklearn.decomposition.PCA
     """
     def __init__(self, n_components=None):
         self.n_components = n_components
 
     def fit_transform(self, X):
+        """Fit the model with X and apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+        """
         if self.n_components is None:
             n_components = X.shape[1]
         else:
@@ -38,16 +65,71 @@ class PCA(BaseEstimator, TransformerMixin):
         return s[:self.n_components] * U[:, :self.n_components]
 
     def fit(self, X):
+        """Compute principal components for X
+
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
         self.fit_transform(X)
         return self
 
     def transform(self, X):
+        """Apply dimensionality reduction on X.
+
+        X is projected on the first principal components previous extracted
+        from a training set.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+        """
         return np.dot(X - self.mean_, self.components_.T)
 
     def inverse_transform(self, X):
+        """Transform data back to its original space.
+
+        Returns an array X_original whose transform would be X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            Data in transformed representation.
+
+        Returns
+        -------
+        X_original : array-like, shape (n_samples, n_features)
+        """
         return self.mean_ + np.dot(X, self.components_)
 
     def reconstruct(self, X):
+        """Reconstruct the data using the PCA model
+
+        This is equivalent to calling transform followed by inverse_transform.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            Data in transformed representation.
+
+        Returns
+        -------
+        X_reconstructed : array-like, shape (n_samples, n_components)
+            Reconstructed version of X
+        """
         return self.inverse_transform(self.transform(X))
 
 
@@ -60,13 +142,34 @@ class WPCA(BaseEstimator, TransformerMixin):
 
     Parameters
     ----------
-    TODO
+    n_components : int (optional)
+        Number of components to keep. If not specified, all components are kept
+
+    xi : float (optional)
+        Degree of weight enhancement.
+
+    regularize : boolean (default=False)
+        If True, use ridge regularization when computing the transform.
 
     Attributes
     ----------
-    mean_ :
-    components_ :
-    explained_variance_ :
+    components_ : array, [n_components, n_features]
+        Principal axes in feature space, representing the directions of
+        maximum variance in the data.
+
+    explained_variance_ : array, [n_components]
+        The amount of variance explained by each of the selected components.
+
+    explained_variance_ratio_ : array, [n_components]
+        Percentage of variance explained by each of the selected components.
+
+    mean_ : array, [n_features]
+        Per-feature empirical mean, estimated from the training set.
+
+    See Also
+    --------
+    - PCA
+    - sklearn.decomposition.PCA
 
     References
     ----------
@@ -79,6 +182,23 @@ class WPCA(BaseEstimator, TransformerMixin):
         self.regularize = regularize
 
     def fit(self, X, weights=None):
+        """Compute principal components for X
+
+        Parameters
+        ----------
+        X: array-like, shape (n_samples, n_features)
+            Training data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        weights: array-like, shape (n_samples, n_features)
+            Non-negative weights encoding the reliability of each measurement.
+            Equivalent to the inverse of the Gaussian errorbar.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
         if self.n_components is None:
             n_components = min(X.shape)
         else:
@@ -106,6 +226,25 @@ class WPCA(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, weights=None):
+        """Apply dimensionality reduction on X.
+
+        X is projected on the first principal components previous extracted
+        from a training set.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        weights: array-like, shape (n_samples, n_features)
+            Non-negative weights encoding the reliability of each measurement.
+            Equivalent to the inverse of the Gaussian errorbar.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+        """
         if weights is None:
             weights = np.ones_like(X)
         Y = np.zeros((X.shape[0], self.components_.shape[0]))
@@ -119,10 +258,57 @@ class WPCA(BaseEstimator, TransformerMixin):
         return Y
 
     def fit_transform(self, X, weights=None):
+        """Fit the model with X and apply the dimensionality reduction on X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            New data, where n_samples in the number of samples
+            and n_features is the number of features.
+
+        weights: array-like, shape (n_samples, n_features)
+            Non-negative weights encoding the reliability of each measurement.
+            Equivalent to the inverse of the Gaussian errorbar.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+        """
         return self.fit(X, weights).transform(X, weights)
 
     def inverse_transform(self, X):
+        """Transform data back to its original space.
+
+        Returns an array X_original whose transform would be X.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            Data in transformed representation.
+
+        Returns
+        -------
+        X_original : array-like, shape (n_samples, n_features)
+        """
         return self.mean_ + np.dot(X, self.components_)
 
     def reconstruct(self, X, weights=None):
+        """Reconstruct the data using the PCA model
+
+        This is equivalent to calling transform followed by inverse_transform.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_components)
+            Data in transformed representation.
+
+        weights: array-like, shape (n_samples, n_features)
+            Non-negative weights encoding the reliability of each measurement.
+            Equivalent to the inverse of the Gaussian errorbar.
+
+        Returns
+        -------
+        X_reconstructed : array-like, shape (n_samples, n_components)
+            Reconstructed version of X
+        """
         return self.inverse_transform(self.transform(X, weights))
